e62a06241091327178cdcb01ea4d79b167cc7c07
Facundo8825 <124110812+Facundo8825@users.noreply.github.com>
2023-01-31 22:56:26 -0300

    Add files via upload

---
+++ funcion.js
@@ -0,0 +1,5 @@
+
+document.getElementById('demo').onclick = function (){
+    console.log("capturar en el evento click");
+    document.getElementById("boton").innerHTML = "Profesionista con 5 años de experiencia en el área de gastronomica en busca de un puesto como chef en la empresa gurmet.srl Deseo utilizar mis habilidades gastronomicas, pensamiento creativo y resolución de problemas para ayudar a que gurmet alcance sus objetivos en el area de produccion y presentacion de nuevos platos europeos"
+}
